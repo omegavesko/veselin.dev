d0a792e78df53dcd3a2d05b9fa8fb2e9f136c4bb
Veselin Romić <omegavesko@gmail.com>
2021-03-14 14:25:35 +0100

    feat: install default Gatsby starter

--- src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export interface IndexPageProps {}
-
-const IndexPage: React.SFC<IndexPageProps> = () => {
-  return <div>uwu?</div>;
-};
-
-export default IndexPage;
