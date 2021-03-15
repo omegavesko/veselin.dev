bd6ac8456084007e4142153a44c678da9598df1d
Veselin Romić <omegavesko@gmail.com>
2021-03-15 15:16:51 +0100

    fix: (hopefully) fix HMR in development

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,27 @@
+const plugins = [
+  `gatsby-plugin-react-helmet`,
+  `gatsby-plugin-image`,
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: `${__dirname}/src/images`,
+    },
+  },
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-sharp`,
+  `gatsby-plugin-postcss`,
+]
+
+if (process.env.NODE_ENV === "production") {
+  plugins.push(`gatsby-plugin-preact`)
+}
+
 module.exports = {
   siteMetadata: {
     title: `Veselin Romić`,
     description: `Turning ☕ into 💾 since 19XX`,
-    author: `@omegavesko`
+    author: `@omegavesko`,
   },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`
-      }
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-postcss`,
-    `gatsby-plugin-preact`
-  ]
+  plugins,
 }
