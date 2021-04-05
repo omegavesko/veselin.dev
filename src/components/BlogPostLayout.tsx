4cb5018cd3d8d3cae548303503697adad7f4c4fa
Veselin Romić <omegavesko@gmail.com>
2021-04-05 17:01:49 +0200

    fix: missing <title>

--- src/components/BlogPostLayout.tsx
+++ src/components/BlogPostLayout.tsx
@@ -10,6 +10,7 @@ import {
   formatDistanceToNow,
   differenceInYears,
 } from "date-fns"
+import { Helmet } from "react-helmet"
 
 const InlineCode: React.FC = ({ children }) => (
   <code
@@ -43,29 +44,34 @@ const BlogPostLayout: React.FC<BlogPostLayoutProps> = ({
   const showDateWarning = differenceInYears(new Date(), date) >= 2
 
   return (
-    <MDXProvider components={mdxComponents}>
-      <h1 className="mb-4 text-5xl text-gray-800 font-medium leading-none dark:text-gray-200">
-        {pageContext.frontmatter.title}
-      </h1>
-      <time
-        className="text-gray-800 dark:text-gray-200"
-        dateTime={pageContext.frontmatter.date}
-      >
-        {format(date, "MMMM do, yyyy")}{" "}
-        <span className="dark:text-gray-400">
-          ({formatDistanceToNow(date, { addSuffix: true })})
-        </span>
-      </time>
-      {showDateWarning && (
-        <div className="mt-8 py-3 px-4 bg-red-900 rounded-xl">
-          Be careful! This post is{" "}
-          <strong className="font-medium">over two years old</strong>. Tech can
-          change really fast, so make sure the advice here is still valid before
-          using it, especially if the post contains code samples.
-        </div>
-      )}
-      <div className={styles.container}>{children}</div>
-    </MDXProvider>
+    <>
+      <Helmet>
+        <title>{pageContext.frontmatter.title}</title>
+      </Helmet>
+      <MDXProvider components={mdxComponents}>
+        <h1 className="mb-4 text-5xl text-gray-800 font-medium leading-none dark:text-gray-200">
+          {pageContext.frontmatter.title}
+        </h1>
+        <time
+          className="text-gray-800 dark:text-gray-200"
+          dateTime={pageContext.frontmatter.date}
+        >
+          {format(date, "MMMM do, yyyy")}{" "}
+          <span className="dark:text-gray-400">
+            ({formatDistanceToNow(date, { addSuffix: true })})
+          </span>
+        </time>
+        {showDateWarning && (
+          <div className="mt-8 py-3 px-4 bg-red-900 rounded-xl">
+            Be careful! This post is{" "}
+            <strong className="font-medium">over two years old</strong>. Tech
+            can change really fast, so make sure the advice here is still valid
+            before using it, especially if the post contains code samples.
+          </div>
+        )}
+        <div className={styles.container}>{children}</div>
+      </MDXProvider>
+    </>
   )
 }
 
