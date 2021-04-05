7b2c4d9a8f287591fb3a1348ed9c725c9304725e
Veselin Romić <omegavesko@gmail.com>
2021-04-05 15:02:33 +0200

    feat: misc. tweaks

--- src/components/BlogPostLayout.tsx
+++ src/components/BlogPostLayout.tsx
@@ -4,10 +4,19 @@ import * as styles from "../styles/blog-post.module.css"
 import CodeBlock from "./blog/CodeBlock"
 import { MDXProvider } from "@mdx-js/react"
 import StandardLink from "./StandardLink"
+import {
+  format,
+  parseISO,
+  formatDistanceToNow,
+  differenceInYears,
+} from "date-fns"
 
 const mdxComponents = {
   pre: CodeBlock,
   a: StandardLink,
+  inlineCode: ({ children }) => (
+    <code className="text-yellow-400">{children}</code>
+  ),
 }
 
 export interface BlogPostLayoutProps {
@@ -23,13 +32,27 @@ const BlogPostLayout: React.FC<BlogPostLayoutProps> = ({
   children,
   pageContext,
 }) => {
+  const date = parseISO(pageContext.frontmatter.date)
+  const showDateWarning = differenceInYears(new Date(), date) >= 2
+
   return (
     <MDXProvider components={mdxComponents}>
       <Layout>
         <h1 className={styles.title}>{pageContext.frontmatter.title}</h1>
         <time dateTime={pageContext.frontmatter.date}>
-          {pageContext.frontmatter.date}
+          {format(date, "MMMM do, yyyy")}{" "}
+          <span className="dark:text-gray-400">
+            ({formatDistanceToNow(date, { addSuffix: true })})
+          </span>
         </time>
+        {showDateWarning && (
+          <div className="mt-8 py-3 px-4 bg-red-900 rounded-xl">
+            Be careful! This post is{" "}
+            <strong className="font-medium">over two years old</strong>. Tech
+            can change really fast, so make sure the advice here is still valid
+            before using it, especially if the post contains code samples.
+          </div>
+        )}
         <div className={styles.container}>{children}</div>
       </Layout>
     </MDXProvider>
